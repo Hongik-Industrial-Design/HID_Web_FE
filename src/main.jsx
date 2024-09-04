7b88445766aecc288b3a8712c1fd0e1761dad2de
DefineX <keaikim21@naver.com>
2024-09-04 20:41:56 +0900

    feat: Add color and font guide with ThemeProvider (#13)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../src/styles/main.scss";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme/theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
