6527bf1e674128b0213bad2e65c1525eb77787ef
DefineX <keaikim21@naver.com>
2024-09-07 12:07:15 +0900

    feat(video): Implement Video Player on Detail page (#19)

--- src/pages/Exhibition/Detail/DetailPage.jsx
+++ src/pages/Exhibition/Detail/DetailPage.jsx
@@ -2,6 +2,8 @@ import { DetailPageContainer } from "./DetailPage.styled";
 
 import Header from "@components/Header/Header";
 import HeroSection from "@components/HeroSection/HeroSection";
+import VideoPlayer from "@components/VideoPlayer/VideoPlayer";
+
 
 const DetailPage = () => {
   return (
@@ -9,6 +11,7 @@ const DetailPage = () => {
       <Header />
       <DetailPageContainer>
         <HeroSection />
+        <VideoPlayer />
       </DetailPageContainer>
     </>
   );
