bfedc1f3873bc3b64b3283fb924b318866e33f92
DefineX <keaikim21@naver.com>
2024-09-07 18:31:17 +0900

    chore: Reorder import syntax

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@styles/theme/theme.js";
@@ -5,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import "@styles/main.scss";
 import App from "./App.jsx";
 import ScrollToTop from "./utils/ScrollToTop.js";
-import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
