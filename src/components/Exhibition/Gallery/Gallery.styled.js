085625c6761440fbe79a1da00d87ef1401e19993
DefineX <keaikim21@naver.com>
2024-09-13 00:51:35 +0900

    style(overflow): Add overflow-x hidden property

--- src/components/Exhibition/Gallery/Gallery.styled.js
+++ src/components/Exhibition/Gallery/Gallery.styled.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const GalleryWrapper = styled.div`
   width: 100%;
   height: ${({ $height }) => $height + 238}px;
-  
+
   padding-right: 64px;
 
   position: relative;
 
-  flex-grow: 1;
+  overflow-x: hidden;
 `;
 
 export const GalleryContainer = styled.div`
@@ -20,6 +20,7 @@ export const GalleryContainer = styled.div`
   width: 100%;
 
   overflow-x: scroll;
+  
   /* scroll-snap-type: x mandatory; // 가로 스냅 스크롤 기능 */
 
   /* // Safari, Chorme 에서 스크롤 바 숨기기
