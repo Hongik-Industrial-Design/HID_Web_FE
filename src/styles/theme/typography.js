3626038ae28e0518b48695aa655d18116ec4e5d6
DefineXX <keaikim21@naver.com>
2024-09-23 01:03:13 +0900

    style(typography): Add Body1 typography and Changed typography name Body to Body2

--- src/styles/theme/typography.js
+++ src/styles/theme/typography.js
@@ -1,61 +1,67 @@
 import { css } from "styled-components";
 
 export const fontStyles = {
-    Display: css`
-      font-style: normal;
-      font-size: 32px;
-      font-weight: 300;
-      line-height: 40px;
-      letter-spacing: -0.25%;
-    `,
-    Header1: css`
-      font-style: normal;
-      font-size: 48px;
-      font-weight: 700;
-      line-height: 56px;
-      letter-spacing: 0%;
-    `,
-    Header2: css`
-      font-style: normal;
-      font-size: 32px;
-      font-weight: 700;
-      line-height: 40px;
-      letter-spacing: 0%;
-    `,
-    Title1: css`
-      font-style: normal;
-      font-size: 28px;
-      font-weight: 500;
-      line-height: 32px;
-      letter-spacing: 0%;
-    `,
-    Title2: css`
-      font-style: normal;
-      font-size: 22px;
-      font-weight: 500;
-      line-height: 28px;
-      letter-spacing: 0%;
-    `,
-    Headline: css`
-      font-style: normal;
-      font-size: 20px;
-      font-weight: 400;
-      line-height: 24px;
-      letter-spacing: 0%;
-    `,
-    Body: css`
-      font-style: normal;
-      font-size: 14px;
-      font-weight: 400;
-      line-height: 22px;
-      letter-spacing: 0%;
-    `,
-  
-    Caption: css`
-      font-style: normal;
-      font-size: 14px;
-      font-weight: 400;
-      line-height: 18px;
-      letter-spacing: 0%;
-    `,
-  };
+  Display: css`
+    font-style: normal;
+    font-size: 32px;
+    font-weight: 300;
+    line-height: 40px;
+    letter-spacing: -0.25%;
+  `,
+  Header1: css`
+    font-style: normal;
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 56px;
+    letter-spacing: 0%;
+  `,
+  Header2: css`
+    font-style: normal;
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 40px;
+    letter-spacing: 0%;
+  `,
+  Title1: css`
+    font-style: normal;
+    font-size: 28px;
+    font-weight: 500;
+    line-height: 32px;
+    letter-spacing: 0%;
+  `,
+  Title2: css`
+    font-style: normal;
+    font-size: 22px;
+    font-weight: 500;
+    line-height: 28px;
+    letter-spacing: 0%;
+  `,
+  Headline: css`
+    font-style: normal;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0%;
+  `,
+  Body1: css`
+    font-style: normal;
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 32px;
+    letter-spacing: 0%;
+  `,
+  Body2: css`
+    font-style: normal;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+    letter-spacing: 0%;
+  `,
+  Caption: css`
+    font-style: normal;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 18px;
+    letter-spacing: 0%;
+  `,
+};
