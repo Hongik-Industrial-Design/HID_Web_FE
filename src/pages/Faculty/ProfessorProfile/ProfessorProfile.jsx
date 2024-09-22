e2d6879ce41777e6dc9fe7fd6c9c0482c70248f3
DefineXX <keaikim21@naver.com>
2024-09-23 01:13:37 +0900

    feat(professorProfile): Create flexbox layout of Professor Profile page and Implement Profile section(left section of the page) (#33)

---
+++ src/pages/Faculty/ProfessorProfile/ProfessorProfile.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import axios from "axios";
+
+import Layout from "@layout/Layout";
+
+import {
+  DetailsContainer,
+  ProfessorProfileHeader,
+  ProfessorProfileMainSection,
+  ProfessorProfilePageTitle,
+  ProfessorProfileWrapper,
+  ProfileContainer,
+  ProfileDescription,
+  ProfileEmail,
+  ProfileImage,
+  ProfileName,
+} from "./ProfessorProfile.styled";
+
+import { BreadscrumbContainer } from "@components/Breadscrumb/Breadscrumb.styled";
+
+import forwardArrow from "@assets/arrows/forward-arrow_triangle.svg";
+
+const ProfessorProfile = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [professorInfo, setProfessorInfo] = useState({});
+
+  const { id } = useParams();
+
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const openNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
+  // Fetching Professor Infos & extracting individual professor info
+  useEffect(() => {
+    const fetchProfessorInfo = async () => {
+      try {
+        const response = await axios.get("/data/professor.json");
+        const fetchedProfessorInfos = response.data;
+        console.log(fetchedProfessorInfos);
+
+        // URL id와 일치하는 professor를 찾아서 저장
+        const selectedProfessor = fetchedProfessorInfos.find(
+          (professor) => professor.id === parseInt(id)
+        );
+
+        console.log(selectedProfessor);
+
+        // 찾은 professor 정보를 상태에 저장
+        setProfessorInfo(selectedProfessor);
+      } catch (error) {
+        console.error("Error occured: ", error);
+      }
+    };
+
+    fetchProfessorInfo();
+  }, [id]); // id가 변경될 때마다 데이터를 다시 가져옴
+
+  return (
+    <Layout isHovered={isHovered} setIsHovered={setIsHovered}>
+      <ProfessorProfileWrapper>
+        <ProfessorProfileHeader>
+          <ProfessorProfilePageTitle>
+            Professor<span>.</span>
+          </ProfessorProfilePageTitle>
+          <BreadscrumbContainer
+            $facultyPage={currentPath.startsWith("/faculty")}
+          >
+            <span>Faculty</span>
+            <img
+              src={forwardArrow}
+              alt="forward-arrow"
+              className="forward-arrow"
+            />
+            <span>Professor</span>
+          </BreadscrumbContainer>
+        </ProfessorProfileHeader>
+        <ProfessorProfileMainSection>
+          <ProfileContainer>
+            <ProfileImage
+              src={`/ProfessorProfile/${professorInfo.image}`}
+              alt={professorInfo.name}
+            />
+            <ProfileName>
+              <h3 className="name">{professorInfo.name}</h3>
+              <div className="role">
+                <p className="title">{professorInfo.title},</p>
+                <p className="specialty">{professorInfo.specialization}</p>
+              </div>
+            </ProfileName>
+            <ProfileEmail
+              onClick={() => openNewTab("mailto: jaeyeop.kim@hongik.ac.kr")}
+            >
+              {professorInfo.contact?.email}
+            </ProfileEmail>
+            <ProfileDescription>{professorInfo.description}</ProfileDescription>
+          </ProfileContainer>
+
+          <DetailsContainer></DetailsContainer>
+        </ProfessorProfileMainSection>
+      </ProfessorProfileWrapper>
+    </Layout>
+  );
+};
+
+export default ProfessorProfile;
---
+++ src/pages/Faculty/ProfessorProfile/ProfessorProfile.styled.js
@@ -0,0 +1,125 @@
+import styled from "styled-components";
+import { colors } from "@styles/theme/colors";
+
+export const ProfessorProfileWrapper = styled.div`
+  width: 100%;
+  margin-top: 121px;
+  padding: 0 158px;
+  padding-bottom: 239px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 64px;
+
+  /* padding: 0 158px 239px 158px; */
+`;
+
+export const ProfessorProfileHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProfessorProfilePageTitle = styled.div`
+  ${({ theme }) => theme.fontStyles.Header1}
+  letter-spacing: -0.28px;
+
+  span {
+    color: ${colors.HID_YellowDot};
+  }
+`;
+
+export const ProfessorProfileMainSection = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  gap: 206px;
+`;
+
+export const ProfileContainer = styled.div`
+  width: 450px;
+
+  display: inline-flex;
+  flex-direction: column;
+
+  flex-shrink: 0;
+`;
+
+export const ProfileImage = styled.img`
+  width: 100%;
+
+  border: 1px solid ${colors.HID_Grayscale[200]};
+  border-radius: 2px;
+`;
+
+export const ProfileName = styled.div`
+  margin-top: 64px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  font-style: normal;
+
+  .name {
+    height: 40px;
+
+    font-size: 40px;
+    font-weight: 600;
+    line-height: 40px;
+
+    color: ${colors.HID_Grayscale[800]};
+  }
+
+  .role {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 3px;
+  }
+
+  .title {
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 32px;
+
+    color: ${colors.HID_Grayscale[900]};
+  }
+
+  .specialty {
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 32px;
+
+    color: ${colors.HID_Grayscale[900]};
+  }
+`;
+
+export const ProfileEmail = styled.a`
+  margin-top: 26px;
+
+  font-style: normal;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 32px;
+
+  color: ${colors.HID_Navy[900]};
+`;
+
+export const ProfileDescription = styled.p`
+  width: 100%;
+  margin-top: 80px;
+
+  font-style: normal;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 32px;
+
+  color: ${colors.HID_Grayscale[700]};
+`;
+
+export const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
