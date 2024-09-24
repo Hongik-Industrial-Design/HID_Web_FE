1188c975bbbdd75c38cb00a0074c29bb4daae379
DefineXX <keaikim21@naver.com>
2024-09-24 23:41:16 +0900

    feat(layout): Implement Flexbox to TopLevelContainer of Layout component to ensure footer sticks to the bottom on each pages (#21)

--- src/layout/Layout.styled.js
+++ src/layout/Layout.styled.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const TopLevelContainer = styled.div`
-    
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
 `
 
 export const Contents = styled.main`
-
+    flex-grow: 1;
 `
