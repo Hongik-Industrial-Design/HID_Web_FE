cb3486bc19cc0dc424be4b2e0b2d76c4d290d7a4
DefineXX <keaikim21@naver.com>
2024-09-24 20:50:50 +0900

    style(facultyList): Modify grid-template-columns value by using minmax function for better UX (#32)

--- src/pages/Faculty/FacultyList/FacultyGallery/FacultyGallery.styled.js
+++ src/pages/Faculty/FacultyList/FacultyGallery/FacultyGallery.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const FacultyGalleryGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(3, minmax(404px, 1fr));
   row-gap: 24px;
   column-gap: 25px;
 `;
