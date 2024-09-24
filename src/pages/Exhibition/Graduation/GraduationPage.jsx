e1ea1349479743263e7b35cec29de325e45b31fd
DefineXX <keaikim21@naver.com>
2024-09-25 02:32:38 +0900

    refactor(graduation): Combined MainBanner component into GraduationPage component  (#37)

--- src/pages/Exhibition/Graduation/GraduationPage.jsx
+++ src/pages/Exhibition/Graduation/GraduationPage.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 
 import Layout from "@layout/Layout";
-import MainBanner from "@components/MainBanner/MainBanner";
 import Exhibition from "@components/Exhibition/Exhibition";
+
+import { GraduationBannerImg } from "./GraduationPage.styled";
+
+import graduationBanner from "@assets/BannerImage/graduation-banner.jpg";
 
 export const GraduationPage = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <Layout isHovered={isHovered} setIsHovered={setIsHovered}>
-      <MainBanner />
+      <GraduationBannerImg src={graduationBanner} alt="main-banner-img" />
       <Exhibition />
     </Layout>
   );
---
+++ src/pages/Exhibition/Graduation/GraduationPage.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const GraduationBannerImg = styled.img`
+  width: 100%;
+  flex-shrink: 0;
+`;
