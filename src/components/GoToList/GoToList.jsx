8019d4649f3201d84080825c008f63930e397050
DefineXX <keaikim21@naver.com>
2024-09-25 15:21:53 +0900

    style(GoToList): Change LeftArrow SVG image to StyledLeftArrow svgr component and Implement animation based on button  hover and click (#37)

--- src/components/GoToList/GoToList.jsx
+++ src/components/GoToList/GoToList.jsx
@@ -1,29 +1,22 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { ButtonContainer } from "./GoToList.styled";
-import leftArrow from "@assets/arrows/left-arrow_full.svg";
-
+import {
+  ButtonContainer,
+  GoToListText,
+  StyledLeftArrow,
+} from "./GoToList.styled";
 
 const GoToList = ({ isHovered, setIsHovered }) => {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(true);
-  };
-
   return (
     <Link to="/graduation">
       <ButtonContainer
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        onClick={handleClick}
         $isHovered={isHovered}
-        $isActive={isActive === true}
       >
-        <img src={leftArrow} alt="left-arrow" className="left-arrow" />
-        <div className="text">Go To List</div>
+        <StyledLeftArrow $isHovered={isHovered} />
+        <GoToListText $isHovered={isHovered}>Go To List</GoToListText>
       </ButtonContainer>
     </Link>
   );
--- src/components/GoToList/GoToList.styled.js
+++ src/components/GoToList/GoToList.styled.js
@@ -1,50 +1,76 @@
 import styled from "styled-components";
 import { colors } from "@styles/theme/colors";
 
+import LeftArrow from "@assets/arrows/left-arrow_full.svg?react";
+
 export const ButtonContainer = styled.div`
-  ${({ $isHovered }) =>
-    $isHovered
-      ? `background-color: #7488c8; color: ${colors.HID_Grayscale[0]};`
-      : "background-color: #D2D2D7A3; color: #0000008F;"};
-
-  ${({ $isActive }) =>
-    $isActive &&
-    `background-color: ${colors.HID_Navy[900]}; color: ${colors.HID_Grayscale[0]};`}
-
-  transition: color, background-color 0.18s ease;
-
   display: flex;
   justify-content: center;
   align-items: center;
 
+  position: relative;
+
   height: 40px;
   padding: 8px 16px;
 
+  background-color: #d2d2d7a3;
   border-radius: 100px;
+
+  transition: background-color 0.4s, transform 0.15s;
 
   cursor: pointer;
 
-  .left-arrow {
-    ${({ $isHovered }) =>
-      $isHovered ? `fill: ${colors.HID_Grayscale[0]};` : "fill: #0000008F;"};
-
-    width: 24px;
-    height: 24px;
+  &:hover {
+    background-color: transparent;
   }
 
-  .text {
-    ${({ $isHovered }) =>
-      $isHovered ? `color: ${colors.HID_Grayscale[0]};` : "color: #0000008F;"};
-
-    ${({ $isActive }) =>
-      $isActive &&
-      `color: ${colors.HID_Grayscale[0]};`}
-
-    transition: color 0.2s ease;
-
-    font-style: normal;
-    font-size: 14px;
-    font-weight: 600;
-    line-height: 22px;
+  &:active {
+    transform: translateY(3px);
   }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #7488c8;
+    border-radius: 100px;
+    transform: translateX(20px) scale(0.1);
+    opacity: 0;
+    z-index: -1;
+    transition: transform 0.3s, opacity 0.3s, background-color 0.3s;
+  }
+
+  &:hover::after {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  &:active::after {
+    background-color: ${colors.HID_Navy[900]};
+  }
+`;
+
+export const GoToListText = styled.div`
+  color: ${({ $isHovered }) =>
+    $isHovered ? `${colors.HID_Grayscale[0]}` : "#0000008F"};
+
+  transition: color 0.4s;
+
+  font-style: normal;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22px;
+`;
+
+export const StyledLeftArrow = styled(LeftArrow)`
+  width: 24px;
+  height: 24px;
+
+  fill: ${({ $isHovered }) =>
+    $isHovered ? `${colors.HID_Grayscale[0]}` : "#0000008F"};
+
+  transition: fill 0.5s;
 `;
