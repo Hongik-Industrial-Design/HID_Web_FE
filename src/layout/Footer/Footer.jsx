7e45ce0e3f7805db739e5c19c918279297412274
DefineXX <keaikim21@naver.com>
2024-11-18 18:13:28 +0900

    fix(Footer): Apply white-space: nowrap to ContactInfos (#7)

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,10 +1,4 @@
-import {
-  ContactInfos,
-  FooterContainer,
-  Infos,
-  SocialIcon,
-  SocialIconContainer,
-} from "./Footer.styled";
+import * as S from "./Footer.styled";
 
 import FooterLogo from "@assets/footer-logo.svg";
 import FacebookLogo from "@assets/Social-Icons/Facebook.svg";
@@ -17,47 +11,43 @@ const Footer = () => {
   };
 
   return (
-    <>
-      <FooterContainer>
-        <ContactInfos>
-          <img src={FooterLogo} alt="footer-logo" style={{ flexShrink: 0 }} />
-          <Infos>
-            <div className="address">
-              04066 94 <br /> Wausan-ro Mapo-gu Seoul Republic of Korea
-            </div>
-            <div className="contact">
-              T. 02-320-1215 <br /> F. 02-324-4265
-            </div>
-            <div className="copyright">
-              Copyright ⓒ by Hongik Industrial Design. All right reserved.
-            </div>
-          </Infos>
-        </ContactInfos>
-        <SocialIconContainer>
-          <SocialIcon
-            src={FacebookLogo}
-            alt="Facebook-logo"
-            onClick={() =>
-              openNewTab(
-                "https://www.facebook.com/groups/hiid2012/?locale=ko_KR"
-              )
-            }
-          />
-          <SocialIcon
-            src={InstagramLogo}
-            alt="Instagram-logo"
-            onClick={() =>
-              openNewTab("https://www.instagram.com/sandidandi_hid/")
-            }
-          />
-          <SocialIcon
-            src={MailLogo}
-            alt="Mail-logo"
-            onClick={() => openNewTab("mailto: webmaster@hongik.ac.kr")}
-          />
-        </SocialIconContainer>
-      </FooterContainer>
-    </>
+    <S.FooterContainer>
+      <S.ContactInfos>
+        <img src={FooterLogo} alt="footer-logo" style={{ flexShrink: 0 }} />
+        <S.Infos>
+          <div className="address">
+            04066 94 <br /> Wausan-ro Mapo-gu Seoul Republic of Korea
+          </div>
+          <div className="contact">
+            T. 02-320-1215 <br /> F. 02-324-4265
+          </div>
+          <div className="copyright">
+            Copyright ⓒ by Hongik Industrial Design. All right reserved.
+          </div>
+        </S.Infos>
+      </S.ContactInfos>
+      <S.SocialIconContainer>
+        <S.FacebookIcon
+          src={FacebookLogo}
+          alt="Facebook-logo"
+          onClick={() =>
+            openNewTab("https://www.facebook.com/groups/hiid2012/?locale=ko_KR")
+          }
+        />
+        <S.InstagramIcon
+          src={InstagramLogo}
+          alt="Instagram-logo"
+          onClick={() =>
+            openNewTab("https://www.instagram.com/sandidandi_hid/")
+          }
+        />
+        <S.MailIcon
+          src={MailLogo}
+          alt="Mail-logo"
+          onClick={() => openNewTab("mailto: webmaster@hongik.ac.kr")}
+        />
+      </S.SocialIconContainer>
+    </S.FooterContainer>
   );
 };
 
