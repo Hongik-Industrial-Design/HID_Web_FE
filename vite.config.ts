c04278b873d4af1eaf940156aef2d09666a1f47e
DefineXX <keaikim21@naver.com>
2025-01-05 21:59:32 +0900

    fix(alias): vite.config.ts 파일 내 절대 경로 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,12 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, '/src/assets'),
-      '@components': path.resolve(__dirname, '/src/components'),
-      '@layout': path.resolve(__dirname, '/src/layout'),
-      '@pages': path.resolve(__dirname, '/src/pages'),
-      '@styles': path.resolve(__dirname, '/src/styles'),
-      '@utils': path.resolve(__dirname, '/src/utils'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
   server: {
