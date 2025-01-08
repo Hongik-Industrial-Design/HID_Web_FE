5f86cd294f6d3bb6b6be26b8c184ed738c149ea3
DefineXX <keaikim21@naver.com>
2025-01-09 02:59:37 +0900

    fix(deploy): 이미지 파일 import문 수정

--- src/pages/Community/Notice/NoticeSection.styled.ts
+++ src/pages/Community/Notice/NoticeSection.styled.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import { colors } from '@styles/theme/colors';
 
-import clipIcon from '@assets/icons/svgs/Notice/paper-clip.svg?react';
-import alarmIcon from '@assets/icons/svgs/Notice/alarm.svg?react';
+import clipIcon from '@assets/icons/svgs/notice/paper-clip.svg?react';
+import alarmIcon from '@assets/icons/svgs/notice/alarm.svg?react';
 
 export const NoticeCategoryContainer = styled.div`
   display: flex;
