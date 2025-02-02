40dbb736048dd555d0b7f4093609236d0e04c9ff
DefineXX <keaikim21@naver.com>
2025-02-02 17:39:49 +0900

    refactor(banner): Banner 이미지 관련 motion.div refactor (#53)

--- src/pages/Exhibition/Graduation/GraduationExhibition.styled.ts
+++ src/pages/Exhibition/Graduation/GraduationExhibition.styled.ts
@@ -1,10 +1,13 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 export const GraduationExhibitionContainer = styled.div`
   width: 100%;
 `;
 
-export const GraduationBannerImg = styled.img`
+export const BannerFrame = styled(motion.div)``;
+
+export const GraduationBanner = styled.img`
   width: 100%;
-  flex-shrink: 0;
+  height: fit-content;
 `;
