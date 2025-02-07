48b1f4e97d1c3a96a8bca9d78fb7f9471bc43ba6
DefineXX <keaikim21@naver.com>
2025-02-08 00:19:10 +0900

    fix(graduation): 졸업 전시 Dropdown 내 전시 포스터 Hover시 Preview 내용 렌더링 로직 추가 (#57)

--- src/layout/Dropdown/Graduation/GraduationTab.tsx
+++ src/layout/Dropdown/Graduation/GraduationTab.tsx
@@ -6,11 +6,46 @@ import { GraduationTabProps } from '../Dropdown.types';
 import * as S from './GraduationTab.styled';
 
 const exhibitionInfos = {
-  title: 'New Normal',
-  description:
-    'Cheems is a modular console that uses generative AI to enable players to create games. It responds to the rising trend of players wanting to shape their own gaming experiences.',
   timeline: [2025, 2024, 2023, 2022, 2021],
+  infos: [
+    {
+      year: 2025,
+      title: 'New Normal',
+      description:
+        'Redefining the standard for a rapidly changing world. This exhibition explores the evolving relationship between society and design, presenting innovative perspectives that reflect the shifting norms of our time.',
+    },
+    {
+      year: 2024,
+      title: 'DESIGN•A',
+      description:
+        'Opening new possibilities in design. This exhibition showcases a harmonious blend of creativity and functionality, pushing the boundaries of conventional design thinking to inspire a sublime and meaningful approach.',
+    },
+    {
+      year: 2023,
+      title: 'Inter-',
+      description:
+        'The aesthetics of connection and interaction. This exhibition delves into the harmony between diverse elements, highlighting the importance of interplay in creating impactful design solutions.',
+    },
+    {
+      year: 2022,
+      title: 'Any Wise',
+      description:
+        'Moments of wise choices in design. Through insightful and thought-provoking works, this exhibition emphasizes the importance of wisdom and intuition in the creative process, celebrating meaningful design decisions.',
+    },
+    {
+      year: 2021,
+      title: 'Without',
+      description:
+        'Creativity born from absence. This exhibition invites viewers into a world of design inspired by what is missing, demonstrating how limitations can give rise to limitless creativity and imaginative solutions.',
+    },
+  ],
 };
+
+interface ExhibitionInfos {
+  year: number;
+  title: string;
+  description: string;
+}
 
 const GraduationTab = ({
   isRendered,
@@ -19,9 +54,29 @@ const GraduationTab = ({
   leaveDropdown,
 }: GraduationTabProps): JSX.Element => {
   const [hoveredYear, setHoveredYear] = useState<number | null>(null);
+  const [dropdownInfos, setDropdownInfos] = useState<
+    ExhibitionInfos | undefined
+  >(exhibitionInfos.infos[0]);
 
   const handleHoveredYear = (year: number | null) => {
     setHoveredYear(year);
+  };
+
+  const handleDropdownInfos = (year: number) => {
+    const hoveredExhibitionInfos = exhibitionInfos.infos.find(
+      (info) => info.year === year
+    );
+
+    setDropdownInfos(hoveredExhibitionInfos);
+  };
+
+  const handleMouseEnter = (year: number) => {
+    handleHoveredYear(year);
+    handleDropdownInfos(year);
+  };
+
+  const handleMouseLeave = () => {
+    handleHoveredYear(null);
   };
 
   return (
@@ -32,10 +87,10 @@ const GraduationTab = ({
       onMouseLeave={() => leaveDropdown()}
     >
       <S.ExhibitionInfos>
-        <S.ExhibitionTitle>{exhibitionInfos.title}</S.ExhibitionTitle>
+        <S.ExhibitionTitle>{dropdownInfos?.title}</S.ExhibitionTitle>
         <S.InfoDivider />
         <S.ExhibitionDescription>
-          {exhibitionInfos.description}
+          {dropdownInfos?.description}
         </S.ExhibitionDescription>
       </S.ExhibitionInfos>
       <S.ExhibitionPosterContainer>
@@ -43,10 +98,10 @@ const GraduationTab = ({
           {exhibitionInfos.timeline.map((year) => (
             <S.ExhibitonPosterItem
               key={year}
-              onMouseEnter={() => handleHoveredYear(year)}
-              onMouseLeave={() => handleHoveredYear(null)}
+              onMouseEnter={handleMouseEnter.bind(null, year)}
+              onMouseLeave={handleMouseLeave}
             >
-              <S.ExhibitonLink href={`/graduation/${year}`}>
+              <S.ExhibitonLink to={`/graduation/${year}`}>
                 <S.ExhibitionPoster
                   src={`/Dropdown-Poster/${year}_DD_Thumbnail.jpg`}
                   alt={`${year}_DD_Thumbnail`}
