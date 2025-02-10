bb5af0f10e347b539310d076b4285a12576a076b
DefineXX <keaikim21@naver.com>
2025-02-10 18:53:28 +0900

    fix(postTypeBox): PostTypeBox 컴포넌트 조건부 스타일링(padding) 추가 (#66)

--- src/components/PostTypeBox/PostTypeBox.styled.ts
+++ src/components/PostTypeBox/PostTypeBox.styled.ts
@@ -1,9 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const PostTypeBoxContainer = styled.div<{ $isNotice: boolean }>`
   width: fit-content;
   height: fit-content;
-  padding: 0.4rem 1.4rem;
 
   display: flex;
   justify-content: center;
@@ -11,8 +10,16 @@ export const PostTypeBoxContainer = styled.div<{ $isNotice: boolean }>`
 
   border-radius: 2px;
 
-  ${({ $isNotice, theme }) =>
-    $isNotice ? theme.fontStyles.Body3 : theme.fontStyles.Headline2}
+  ${({ $isNotice }) =>
+    $isNotice
+      ? css`
+          ${({ theme }) => theme.fontStyles.Body3}
+          padding: 0.4rem 1.6rem;
+        `
+      : css`
+          ${({ theme }) => theme.fontStyles.Body3}
+          padding: 0.4rem 1.4rem;
+        `}
 
   color: ${({ theme }) => theme.colors.HID_Navy[900]};
   background-color: #e7eaf5;
