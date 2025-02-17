3aacbda99abf271f915947b13293c2f0f10e52b0
DefineXX <keaikim21@naver.com>
2025-02-18 03:30:43 +0900

    feat(axios): .env.test 내 테스트 서버 Base URL 추가 및 Axios Instance 생성 (#73)

---
+++ src/api/axios.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL_HWISO,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
