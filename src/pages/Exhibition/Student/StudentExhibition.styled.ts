ebd2477df06c7c7c72704d9be31a4d570c74a3e6
DefineXX <keaikim21@naver.com>
2025-02-18 19:15:23 +0900

    fix(student): 학생 전시 Preview 데이터 Fetching 구현 및 컴포넌트 분리 방식 변경 (#73)

--- src/pages/Exhibition/Student/StudentExhibition.styled.ts
+++ src/pages/Exhibition/Student/StudentExhibition.styled.ts
@@ -6,3 +6,38 @@ export const StudentExhibitionContainer = styled.div`
   margin-top: 70px;
   padding: 6.8rem 0 8rem 0;
 `;
+
+export const ExhibitionContainer = styled.div`
+  width: 100%;
+  height: fit-content;
+  /* padding: 11.7rem 0; */
+
+  display: flex;
+  justify-content: center;
+  gap: 19.6rem;
+`;
+
+export const StickyContainer = styled.div`
+  position: relative;
+`;
+
+export const StudentExhibitionGalleryContainer = styled.div`
+  width: fit-content;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  gap: 8.8rem;
+`;
+
+export const StudentExhibitionGalleryTitle = styled.h1`
+  ${({ theme }) => theme.fontStyles.Header1};
+  color: ${({ theme }) => theme.colors.HID_Grayscale[800]};
+
+  span {
+    color: ${({ theme }) => theme.colors.HID_YellowDot};
+
+    user-select: auto;
+    -webkit-user-select: auto;
+  }
+`;
--- src/pages/Exhibition/Student/StudentExhibition.tsx
+++ src/pages/Exhibition/Student/StudentExhibition.tsx
@@ -1,13 +1,95 @@
 import { JSX } from 'react/jsx-runtime';
+import { useEffect, useState } from 'react';
 
-import Exhibition from '@components/Exhibition/Exhibition';
+import { fetchStudentExhibitionPreview } from '@api/exhibition';
+import { StudentExhibitionPreview } from '@schemas/exhibition';
+
+import Category from '@components/Category/Category';
+import StudentExhibitionGallery from '@components/Exhibition/StudentGallery/StudentExhibitionGallery';
 
 import * as S from './StudentExhibition.styled';
 
+export interface SelectStudentExhibition {
+  year: string;
+  club: string;
+}
+
 const StudentExhibiton = (): JSX.Element => {
+  const [selectedExhibition, setSelectedExhibition] =
+    useState<SelectStudentExhibition>({
+      year: '2024',
+      club: 'ALL',
+    });
+
+  const handlePreviewFilter = (
+    key: keyof SelectStudentExhibition,
+    club: string
+  ) =>
+    setSelectedExhibition((prev) => ({
+      ...prev,
+      [key]: club,
+    }));
+
+  const [previews, setPreviews] = useState<StudentExhibitionPreview>([]);
+  // const [filteredPreviews, setFilteredPreviews] =
+  //   useState<StudentExhibitionPreview>([]);
+
+  // Fetching Student Exhibition Preview Data
+  useEffect(() => {
+    const getStudentExhibitionPreview = async () => {
+      try {
+        const studentPreview = await fetchStudentExhibitionPreview(
+          selectedExhibition.year,
+          selectedExhibition.club
+        );
+        console.log(
+          '타입 검증 후의 학생 전시 Preview 데이터: ',
+          studentPreview
+        );
+
+        setPreviews(studentPreview);
+        // setFilteredPreviews(studentPreview);
+      } catch (error) {
+        console.error('학생 전시 데이터 가져오기 실패: ', error);
+      }
+    };
+
+    getStudentExhibitionPreview();
+  }, [selectedExhibition]);
+
+  // // Client Side Filtering Logic (애니메이션 구체화 시 사용 - 백엔드와 로직 상의 필요)
+  // const handlePreviewFilter = (category: string) => {
+  //   if (category === 'ALL') {
+  //     setFilteredPreviews(previews);
+  //   } else {
+  //     const filteredPreviews = previews.filter(
+  //       (preview) => preview.club === category
+  //     );
+
+  //     setFilteredPreviews(filteredPreviews);
+  //   }
+  // };
+
   return (
     <S.StudentExhibitionContainer>
-      <Exhibition />
+      <S.ExhibitionContainer>
+        <S.StickyContainer>
+          <Category
+            currentCategory={selectedExhibition.club}
+            handleFilter={handlePreviewFilter}
+          />
+        </S.StickyContainer>
+        <S.StudentExhibitionGalleryContainer>
+          <S.StudentExhibitionGalleryTitle>
+            Student Exhibiton<span>.</span>
+          </S.StudentExhibitionGalleryTitle>
+
+          <StudentExhibitionGallery
+            pieces={previews}
+            exhibitionYear={selectedExhibition.year}
+          />
+        </S.StudentExhibitionGalleryContainer>
+      </S.ExhibitionContainer>
     </S.StudentExhibitionContainer>
   );
 };
