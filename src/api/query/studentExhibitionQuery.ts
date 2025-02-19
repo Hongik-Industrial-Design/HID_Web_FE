4d62a159ba0fc95fc148474ace691fa6aee79e45
DefineXX <keaikim21@naver.com>
2025-02-20 01:56:37 +0900

    fix(api): @tanstack/query/exhaustive-deps 규칙에 따른 queryKey 수정 (#79)

--- src/api/query/studentExhibitionQuery.ts
+++ src/api/query/studentExhibitionQuery.ts
@@ -6,19 +6,18 @@ import {
 } from '@api/exhibition';
 
 const STUDENT_QUERY_KEYS = {
-  preview: (year: string) => ['Student', year],
+  preview: (year: string, club: string) => ['Student', year, club],
   detail: (year: string, exhibitId: number) => ['Student', year, exhibitId],
 };
 
 export const useStudentExhbitionPreviewQuery = (year: string, club: string) => {
   return useQuery({
-    queryKey: STUDENT_QUERY_KEYS.preview(year),
+    queryKey: STUDENT_QUERY_KEYS.preview(year, club), // queryFn에서 사용된 인자들이 queryKey에 포함되어야 함 (@tanstack/query/exhaustive-deps 참고)
     queryFn: () => fetchStudentExhibitionPreview(year, club),
     staleTime: 1000 * 10, // 10초 (Testing)
     gcTime: 1000 * 60, // 1분 (Testing)
   });
 };
-
 export const useStudentExhibitionDetailQuery = (
   year: string,
   exhibitId: number
