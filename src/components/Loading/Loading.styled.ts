a2d5351623b87caa4a02fbfa9fb7381ce392c851
DefineXX <keaikim21@naver.com>
2025-02-20 15:29:10 +0900

    feat(loading): API 요청 처리 중일 때 사용할 Loading 컴포넌트 구현 (#76)

---
+++ src/components/Loading/Loading.styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const LoadingContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-top: 3.2rem;
+`;
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+import * as S from './Loading.styled';
+
+const Loading = () => {
+  return <S.LoadingContainer>Loading...</S.LoadingContainer>;
+};
+
+export default Loading;
