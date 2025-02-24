1999d3f40671083086d153f351ded1c0fc532827
DefineXX <keaikim21@naver.com>
2025-02-25 03:26:49 +0900

    chore(setup): 모노레포 구조로 인한 eslint 관련 중복 개발 의존성 삭제 및 tsconfig.app.json 내 base config 확장 (#92)

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@api': path.resolve(__dirname, './src/api'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@icons': path.resolve(__dirname, './src/icons'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@schemas': path.resolve(__dirname, './src/schemas'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  server: {
+    port: 4000,
+  },
 });
