e4aadf50437debe39c1a4016e749bb01a9e86017
DefineXX <keaikim21@naver.com>
2025-02-25 20:25:57 +0900

    feat(svgr): @shared/ui 내 svgr 변환 위해 esbuild-svgr-plugin 설치 밎 적용 (#94)

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup';
+import svgrPlugin from 'esbuild-plugin-svgr';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -6,4 +7,5 @@ export default defineConfig({
   dts: true,
   sourcemap: true,
   clean: true,
+  esbuildPlugins: [svgrPlugin()],
 });
