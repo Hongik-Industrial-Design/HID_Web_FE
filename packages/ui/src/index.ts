6632aa13a15d32f65d57624f7a2b0f284f75ca01
DefineXX <keaikim21@naver.com>
2025-02-27 16:45:03 +0900

    fix: 관리자 페이지 UI 변경으로 인한 공통 UI 패키지 삭제 (#94)

--- packages/ui/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Header } from '../src/components/Header/Header';
-export { default as Footer } from '../src/components/Footer/Footer';
-export { default as Dropdown } from '../src/components/Dropdown/Dropdown';
