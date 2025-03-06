bc2e888f95f57974ed4c58f322d5f7c427b30b79
DefineXX <keaikim21@naver.com>
2025-03-06 11:51:37 +0900

    feat: 이미지 추가 Box 구현 (#96)

--- apps/admin/src/icons/Plus.ts
+++ apps/admin/src/icons/Plus.ts
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
 
 import plusIcon from '@assets/svgs/plus.svg?react';
+import plusIconNavy from '@assets/svgs/plus-navy.svg?react';
 
 export const PlusIcon = styled(plusIcon)`
   width: 2rem;
   height: fit-content;
+`;
+
+export const PlusIconNavy = styled(plusIconNavy)`
+  width: 2rem;
+  height: fit-content;
+
+  path {
+    fill: ${({ theme }) => theme.colors.HID_Navy[900]};
+  }
 `;
