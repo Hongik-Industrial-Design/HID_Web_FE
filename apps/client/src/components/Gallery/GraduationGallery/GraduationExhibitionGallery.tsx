7e115e364d0b60b6a8577c5897064e6c111e6d99
DefineXX <keaikim21@naver.com>
2025-03-10 15:26:08 +0900

    fix: 학생 전시 페이지와 동일한 구조로 졸업 전시 페이지 구조 수정 (#103)

--- apps/client/src/components/Gallery/GraduationGallery/GraduationExhibitionGallery.tsx
+++ apps/client/src/components/Gallery/GraduationGallery/GraduationExhibitionGallery.tsx
@@ -1,6 +1,5 @@
 import { JSX } from 'react/jsx-runtime';
 import { useMemo, useState } from 'react';
-import { useNavigate } from 'react-router';
 import { AnimatePresence } from 'framer-motion';
 
 import { GalleryInfos } from '../Exhibition.types';
@@ -9,19 +8,17 @@ import SearchBar from '@components/SearchBar/SearchBar';
 import Piece from './Piece/Piece';
 import Pagination from '@components/Pagination/Pagination';
 
-import * as S from './ExhibitionGallery.styled';
+import * as S from './GraduationExhibitionGallery.styled';
 
 interface GalleryProps {
   pieces: GalleryInfos[];
   exhibitionYear: string;
 }
 
-const ExhibitionGallery = ({
+const GraduationExhibitionGallery = ({
   pieces,
   exhibitionYear,
 }: GalleryProps): JSX.Element => {
-  const navigate = useNavigate();
-
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   const totalPages = Math.ceil(pieces.length / 9);
@@ -34,8 +31,6 @@ const ExhibitionGallery = ({
     return pieces.slice(startIndex, currentPage * 9);
   }, [currentPage, pieces]);
 
-  const goToDetailPage = (id: number) => navigate(`/graduation/work/${id}`);
-
   return (
     <S.GalleryWrapper>
       <S.GalleryHeader>
@@ -46,10 +41,11 @@ const ExhibitionGallery = ({
         <AnimatePresence>
           {paginatedPieces.map((piece) => (
             <Piece
-              key={piece.id}
-              pieceName={piece.thumbnail}
-              pieceInfos={piece.credit}
-              goToDetailPage={() => goToDetailPage(piece.id)}
+              key={piece.exhibitId}
+              exhibitId={piece.exhibitId}
+              title={piece.titleEn}
+              subTitle={piece.subTitleEn}
+              imageURL={piece.mainImgUrl}
             />
           ))}
         </AnimatePresence>
@@ -65,4 +61,4 @@ const ExhibitionGallery = ({
   );
 };
 
-export default ExhibitionGallery;
+export default GraduationExhibitionGallery;
--- apps/client/src/pages/Exhibition/Graduation/GraduationExhibition.tsx
+++ apps/client/src/pages/Exhibition/Graduation/GraduationExhibition.tsx
@@ -1,20 +1,62 @@
 import { JSX } from 'react/jsx-runtime';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import { useGraduationBannerVideoQuery } from '@api/query/graduationExhibitionQuery';
+import { GalleryInfos } from '@components/Gallery/Exhibition.types';
 
-import Exhibition from '@components/Exhibition/Exhibition';
+import { GRADUATION_CATEGORY_LIST } from '@constants/exhibitionCategory';
+
 import Loading from '@components/Loading/Loading';
+import Category from '@components/Category/Category';
+import GraduationExhibitionGallery from '@components/Gallery/GraduationGallery/GraduationExhibitionGallery';
 
 import * as S from './GraduationExhibition.styled';
 
 export const GraduationExhibition = (): JSX.Element => {
-  const exhibitionYear = 2024;
+  const [pieces, setPieces] = useState<GalleryInfos[]>([]); // All Pieces
+  const [categorizedPieces, setCategorizedPieces] = useState<GalleryInfos[]>(
+    []
+  );
+  const [exhibitionYear, setExhibitionYear] = useState<number>(2024);
 
+  // Fetching Banner Video
   const {
     status,
     data: bannerVideo,
     error,
-  } = useGraduationBannerVideoQuery(exhibitionYear);
+  } = useGraduationBannerVideoQuery(2024);
+
+  // Fetching Dummy Gallery Image
+  useEffect(() => {
+    const fetchGalleryImage = async () => {
+      try {
+        const response = await axios.get('/data/gallery.json');
+        const galleryData = response.data;
+
+        setPieces(galleryData.gallery);
+        setCategorizedPieces(galleryData.gallery);
+        setExhibitionYear(galleryData.year);
+      } catch (error) {
+        console.error('Fetching Error: ', error);
+      }
+    };
+
+    fetchGalleryImage();
+  }, []);
+
+  // Filterirng corresponding category Pieces list
+  const handleFilterPieces = (category: string) => {
+    if (category === 'All') {
+      setCategorizedPieces(pieces);
+    } else {
+      const filteredPieces = pieces.filter(
+        (piece) => piece.category === category
+      );
+
+      setCategorizedPieces(filteredPieces);
+    }
+  };
 
   return (
     <S.GraduationExhibitionContainer>
@@ -42,7 +84,19 @@ export const GraduationExhibition = (): JSX.Element => {
       </S.BannerFrame>
 
       <S.GraduationExhibitonGalleryContainer>
-        <Exhibition />
+        <S.ExhibitionContainer>
+          <S.StickyContainer>
+            <Category
+              currentCategory={GRADUATION_CATEGORY_LIST[0]}
+              handleFilter={handleFilterPieces}
+            />
+          </S.StickyContainer>
+
+          <GraduationExhibitionGallery
+            pieces={categorizedPieces}
+            exhibitionYear={String(exhibitionYear)}
+          />
+        </S.ExhibitionContainer>
       </S.GraduationExhibitonGalleryContainer>
     </S.GraduationExhibitionContainer>
   );
