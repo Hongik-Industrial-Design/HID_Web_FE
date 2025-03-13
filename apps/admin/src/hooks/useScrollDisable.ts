02c1ad0830b64b80b4a3bc6331f7bbce092b76b2
DefineXX <keaikim21@naver.com>
2025-03-14 02:16:24 +0900

    feat: Skeleton UI를 위한 useScrollDisable 훅 구현 (#116)

---
+++ apps/admin/src/hooks/useScrollDisable.ts
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+
+export const useScrollDisable = () => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+};
