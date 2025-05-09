d2122560019e782bd175e4f465a022b200a9b301
DefineXX <keaikim77@gmail.com>
2025-05-09 20:58:47 +0900

    feat: Faculty Drawer 반응형 v1.1

--- apps/client/src/components/Drawer/Drawer.styled.ts
+++ apps/client/src/components/Drawer/Drawer.styled.ts
@@ -6,7 +6,7 @@ export const DrawerContainer = styled.div<{ $isOpen: boolean }>`
   left: 0;
   z-index: 10000;
 
-  width: clamp(48rem, 32vw, 62rem);
+  width: clamp(56rem, 48vw, 72rem);
   height: 100vh;
 
   display: flex;
@@ -25,6 +25,11 @@ export const DrawerContainer = styled.div<{ $isOpen: boolean }>`
           transform: translateX(-100%);
           transition: transform 0.25s ease;
         `}
+
+  @media (max-width: 768px) {
+    width: 100vw;
+    height: 100vh;
+  }
 `;
 
 export const DrawerHeader = styled.header`
@@ -42,23 +47,56 @@ export const DrawerCloseButton = styled.button`
 `;
 
 export const DrawerContents = styled.main`
+  flex-grow: 1;
+
   width: 100%;
-  height: fit-content;
-  padding: 0.8rem 8rem;
+  height: 100%;
+  padding: 0.8rem 8rem 6.4rem;
 
   display: flex;
   flex-direction: column;
   gap: 4.8rem;
+
+  overflow-y: auto;
 `;
 
 export const DrawerTitle = styled.h1`
   ${({ theme }) => theme.fontStyles.Title1}
+  line-height: normal;
   color: ${({ theme }) => theme.colors.HID_Grayscale[0]};
+
+  @media (max-width: 2560px) {
+    font-size: clamp(4.4rem, 2.6vw, 4.8rem);
+  }
+
+  @media (max-width: 1920px) {
+    font-size: clamp(4.4rem, 2.8vw, 4.8rem);
+  }
+
+  @media (max-width: 1440px) {
+    font-size: clamp(4rem, 3vw, 4.4rem);
+  }
 `;
 
 export const DrawerDescription = styled.p`
+  flex-grow: 1;
+  width: 100%;
+  height: 100%;
+
+  overflow-y: auto;
+
   ${({ theme }) => theme.fontStyles.Body1}
+  font-size: clamp(2rem, 1vw, 2.2rem);
+  line-height: 1.6;
   color: ${({ theme }) => theme.colors.HID_Grayscale[0]};
+
+  @media (max-width: 1920px) {
+    font-size: clamp(1.8rem, 1.2vw, 2rem);
+  }
+
+  @media (max-width: 768px) {
+    font-size: clamp(1.8rem, 3.2vw, 2.2rem);
+  }
 `;
 
 export const DrawerBackground = styled.div<{ $isOpen: boolean }>`
