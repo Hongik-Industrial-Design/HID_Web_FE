bdc078246931fdbffdb968c8e32622e7779998ff
DefineXX <keaikim77@gmail.com>
2025-05-11 23:38:30 +0900

    fix: Path alias 관리 방식 수정

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,40 +1,12 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
-import path from 'path';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      '@api': path.resolve(__dirname, './src/api'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@icons': path.resolve(__dirname, './src/icons'),
-      '@layout': path.resolve(__dirname, './src/layout'),
-      '@lib': path.resolve(__dirname, './src/lib'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@schemas': path.resolve(__dirname, './src/schemas'),
-      '@stores': path.resolve(__dirname, './src/stores'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-    },
-  },
+  plugins: [react(), svgr(), tsconfigPaths()],
   server: {
     port: 3000,
-    // proxy: {
-    //   '/youtube': {
-    //     target: 'https://www.youtube.com',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // },
   },
 });
